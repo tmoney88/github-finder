06eecbb34921f1b02f6d061254feb3f1a4a7aafb
Tanner Gates <tgates4@dmail.dixie.edu>
2021-12-24 08:12:27 -0700

    Create UserItem component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
   const [users, setUsers] = useState([])
   const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    fetchUsers()
-  }, [])
 
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
@@ -22,11 +19,15 @@ function UserResults() {
     setLoading(false)
   }
 
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
   if (!loading) {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map(user => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
