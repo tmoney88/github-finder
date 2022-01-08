62635a558cea537a714d3ed0c2e6b0d8b1bd4979
Tanner Gates <tgates4@dmail.dixie.edu>
2022-01-08 12:05:31 -0700

    Add GithubReducer and clean up fetchUsers function

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer } from 'react'
+import GithubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
@@ -6,10 +7,15 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const initialState = {
+    users: [],
+    loading: false
+  }
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState)
 
   const fetchUsers = async () => {
+    setLoading()
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`
@@ -18,11 +24,15 @@ export const GithubProvider = ({ children }) => {
 
     const data = await response.json()
 
-    setUsers(data)
-    setLoading(false)
+    dispatch({
+      type: 'GET_USERS',
+      payload: data
+    })
   }
 
-  return <GithubContext.Provider value={{ users, loading, fetchUsers }}>{children}</GithubContext.Provider>
+  const setLoading = () => dispatch({ type: 'SET_LOADING' })
+
+  return <GithubContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers }}>{children}</GithubContext.Provider>
 }
 
 export default GithubContext
